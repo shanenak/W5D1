2e5fcd0a77d9e989265f2f4276ab3019d09ebe19
Andoniank <andoniank@gmail.com>
2023-08-14 16:26:53 -0700

    Implement hashing.rb

--- practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
+++ practice-for-ch-ruby-hash-map-lru-cache-long-practice-main/lib/p02_hashing.rb
@@ -4,11 +4,14 @@ end
 
 class Array
   def hash
+    self.empty? ? length : length + first
   end
 end
 
 class String
   def hash
+    alphabet = ("a".."z").to_a
+    self.empty? ? length : length + alphabet.index(self[0].downcase)
   end
 end
 
@@ -16,6 +19,7 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    alphabet = ("a".."z").to_a
+    self.empty? ? length : length + alphabet.index(self.values[0].downcase)
   end
 end
